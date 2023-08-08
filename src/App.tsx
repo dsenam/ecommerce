0750fb5d4585fdaa7d10ddd7793c540b7731d8b9
“Douglas <dsena.m@hotmail.com>
2023-08-07 22:56:40 -0300

    setup: styled components and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -8,10 +8,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 }
